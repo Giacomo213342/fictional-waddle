3ea945a04de281a3923ff56a0a41a48d07e6a81d
The one with the braid <info@braid.business>
2024-08-07 07:56:48 +0000

    chore: refactor message tile

---
+++ lib/src/pages/room/components/event/components/edit_tooltip.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:matrix/matrix.dart';
+
+import '../../../../../../l10n/generated/app_localizations.dart';
+import '../../../../../widgets/human_date.dart';
+
+class EditTooltip extends StatelessWidget {
+  const EditTooltip({super.key, required this.editEvent});
+
+  final Event editEvent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tooltip(
+      message: editEvent.originServerTs
+              .isAfter(DateTime.now().subtract(const Duration(days: 1)))
+          ? AppLocalizations.of(context).editedToday(editEvent.originServerTs)
+          : AppLocalizations.of(context).editedAt(
+              editEvent.originServerTs.humanShortDate(context: context),
+            ),
+      child: const Icon(Icons.edit),
+    );
+  }
+}
