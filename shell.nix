9f209ba10c79b7953f941bdae3b221aa91e7d824
The one with the braid <info@braid.business>
2025-01-15 12:01:00 +0000

    chore: bump version

--- shell.nix
+++ shell.nix
@@ -32,7 +32,7 @@ in mkShell {
   shellHook =
   ''
     # configure the dependency cache persistent
-    export FLUTTER_VERSION="3.27.1"
+    export FLUTTER_VERSION="3.27.2"
     export FLUTTER_HOME="$HOME/build/flutter-$FLUTTER_VERSION"
     # export GEM_HOME="$HOME/build/gem"
 
