41c68965dd53d9a3ffbdaf501eeecc9e157b0d4a
The one with the braid <info@braid.business>
2025-01-15 22:30:52 +0000

    fix: support stopping animated bitmap images

--- lib/src/widgets/mimed_image.dart
+++ lib/src/widgets/mimed_image.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 import 'dart:typed_data';
+import 'dart:ui' as ui show Image;
+import 'dart:ui' hide Image;
 
 import 'package:flutter/material.dart';
 
@@ -55,15 +57,29 @@ class MimedImage extends StatelessWidget {
     return _bitmapImage(bytes);
   }
 
-  Image _bitmapImage(Uint8List bytes) {
-    return Image.memory(
-      bytes,
-      gaplessPlayback: true,
-      fit: fit ?? BoxFit.cover,
-      width: width,
-      height: height,
-    );
-  }
+  // analyzes the frames of a bitmap and either just returns the image or its
+  // first frame in case its animated and the user requests to avoid animations
+  Widget _bitmapImage(Uint8List bytes) => FutureBuilder<ImageFutureResponse>(
+        future: _buildImageFrames(bytes),
+        builder: (context, snapshot) {
+          final frames = snapshot.data;
+          return switch (frames) {
+            AnimatedThumbnailImageProviderFutureResponse() =>
+              AnimationEnabledBuilder(
+                builder: (context, animate) {
+                  if (animate) {
+                    return _buildBitmap(frames.imageProvider);
+                  } else {
+                    return _buildRawBitmap(frames.thumbnail);
+                  }
+                },
+                iconSize: (height ?? 96) / 2.5,
+              ),
+            ImageProviderFutureResponse() => _buildBitmap(frames.imageProvider),
+            _ => _buildRawBitmap(null),
+          };
+        },
+      );
 
   SvgPicture _svgImage(Uint8List bytes) {
     return SvgPicture.memory(
@@ -90,6 +106,44 @@ class MimedImage extends StatelessWidget {
     );
   }
 
+  Future<ImageFutureResponse> _buildImageFrames(Uint8List bytes) async {
+    final provider = MemoryImage(bytes);
+
+    final codec = await instantiateImageCodecWithSize(
+      await ImmutableBuffer.fromUint8List(bytes),
+    );
+    if (codec.frameCount > 1) {
+      final frame = await codec.getNextFrame();
+      return AnimatedThumbnailImageProviderFutureResponse(
+        thumbnail: frame.image,
+        imageProvider: provider,
+      );
+    } else {
+      return ImageProviderFutureResponse(provider);
+    }
+  }
+
+  RawImage _buildRawBitmap(ui.Image? image) {
+    return RawImage(
+      image: image,
+      fit: fit ?? BoxFit.cover,
+      width: width,
+      height: height,
+      isAntiAlias: true,
+    );
+  }
+
+  Image _buildBitmap<T extends Object>(ImageProvider<T> provider) {
+    return Image(
+      image: provider,
+      gaplessPlayback: true,
+      fit: fit ?? BoxFit.cover,
+      width: width,
+      height: height,
+      isAntiAlias: true,
+    );
+  }
+
   Future<LottieComposition?> _lottieDecoder(List<int> bytes) async =>
       // Telegram decoder
       await LottieComposition.decodeGZip(
@@ -107,4 +161,24 @@ class MimedImage extends StatelessWidget {
               file.name.endsWith('.json'),
         ),
       );
+}
+
+abstract class ImageFutureResponse {
+  const ImageFutureResponse();
+}
+
+class ImageProviderFutureResponse extends ImageFutureResponse {
+  const ImageProviderFutureResponse(this.imageProvider);
+
+  final ImageProvider imageProvider;
+}
+
+class AnimatedThumbnailImageProviderFutureResponse
+    extends ImageProviderFutureResponse {
+  const AnimatedThumbnailImageProviderFutureResponse({
+    required this.thumbnail,
+    required ImageProvider imageProvider,
+  }) : super(imageProvider);
+
+  final ui.Image thumbnail;
 }
