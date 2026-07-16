e24325ebc44e3fe897a0f3548586b142265709e5
Giacomo <giacomonagali07@example.com>
2026-07-16 18:39:13 +0200

    fix: close client tab slot layout

--- lib/src/widgets/matrix/client_manager/client_tab_view.dart
+++ lib/src/widgets/matrix/client_manager/client_tab_view.dart
@@ -55,7 +55,7 @@ class ClientTabView extends StatelessWidget {
                 builder: (context) => child,
               ),
             },
-          },
+          ),
           topNavigation: SlotLayout(
             config: {
               Breakpoints.mediumLargeAndUp: SlotLayout.from(
